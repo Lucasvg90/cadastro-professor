cd87e82fab6396c2982beb8cf2a9e6a3a95fd7eb
lucasvg90 <lucastomsaw@gmail.com>
2026-05-11 22:33:52 -0300

    commit inicial

---
+++ src/main/java/br/senac/tads/dsw/projeto/ProjetoApplication.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 0cb26bd0ae3ef9790b07886f3d9562a5b89c1cb5f778708932ca0117ae5b3a77
+package br.senac.tads.dsw.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/tads/dsw/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 0cb26bd0ae3ef9790b07886f3d9562a5b89c1cb5f778708932ca0117ae5b3a77
+package br.senac.tads.dsw.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
